59c74455018c1bcfd70ed9dbc751d084a51a1843
junhee <joooonh12@gmail.com>
2023-05-31 20:15:17 +0900

    converter, formatter

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,9 +1,24 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort = " + ipPort.getIp());
+        System.out.println("ipPort = " + ipPort.getPort());
+        return "ok";
+    }
 
 }
